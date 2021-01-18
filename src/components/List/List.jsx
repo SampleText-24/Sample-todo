6d18827ef4c30f6d4193e1c0edcb089908513f7d
Alexey Sokolov <64545347+SampleText-24@users.noreply.github.com>
2021-01-18 21:44:01 +0700

    Created a component List

---
+++ src/components/List/List.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import listSvg from "../assets/img/list.svg"
+
+
+const List = ({ items }) => {
+    return (
+        <ul className="todo__list">
+            {items.map(item => (
+                <li className='active'>
+                    <i>
+                    {item.icon ? (
+                        item.icon
+                    ) : (
+                        <i className={`badge badge--${item.color}`}></i>
+                    )}
+                    </i>
+                    <span>{item.name}</span>
+                </li>
+            ))}
+        </ul>
+    )
+}
+
+export default List;
